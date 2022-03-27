fc6c9f97967a6b3a81ea93844679ea8ec4d4addc
b150005 <b150005@outlook.jp>
2022-03-27 14:00:06 +0900

    Initial Commit

---
+++ MVVMSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMSample
+//
+//  Created by 伊藤 直輝 on 2022/03/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
